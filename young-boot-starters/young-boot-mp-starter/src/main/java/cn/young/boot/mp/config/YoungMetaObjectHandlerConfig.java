f8fc5fb6101e490121e19adebfbade5a3e5f792a
Melo <meiko_ooo@163.com>
2025-01-25 13:35:19 +0800

    feat: Mp自动填充功能

---
+++ young-boot-starters/young-boot-mp-starter/src/main/java/cn/young/boot/mp/config/YoungMetaObjectHandlerConfig.java
@@ -0,0 +1,27 @@
+package cn.young.boot.mp.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Mole. meiko_ooo@163.com
+ * @since 2025/1/25 13:04
+ */
+@Slf4j
+@Component
+public class YoungMetaObjectHandlerConfig implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createdAt", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "updatedAt", LocalDateTime.class, LocalDateTime.now());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updatedAt", LocalDateTime.class, LocalDateTime.now());
+    }
+}
