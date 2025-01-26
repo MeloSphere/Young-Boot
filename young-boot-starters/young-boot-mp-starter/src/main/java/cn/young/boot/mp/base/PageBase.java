a018765b8e9a21102fb499afe30298a818db57ce
Melo <meiko_ooo@163.com>
2025-01-26 21:55:53 +0800

    feat: 分页数据

---
+++ young-boot-starters/young-boot-mp-starter/src/main/java/cn/young/boot/mp/base/PageBase.java
@@ -0,0 +1,17 @@
+package cn.young.boot.mp.base;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author Mole. meiko_ooo@163.com
+ * @since 2025/1/26 21:49
+ */
+@Data
+public class PageBase implements Serializable {
+
+    private Long currentPage = 1L;
+
+    private Long pageSize = 10L;
+}
---
+++ young-boot-starters/young-boot-mp-starter/src/main/java/cn/young/boot/mp/base/PageData.java
@@ -0,0 +1,36 @@
+package cn.young.boot.mp.base;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author Mole. meiko_ooo@163.com
+ * @since 2025/1/26 21:33
+ */
+@Getter
+@Setter
+public class PageData<T> extends PageBase {
+
+    private List<T> records;
+
+    private Long total;
+
+
+    public static <T> PageData<T> toYoungPageData(IPage<T> page) {
+        if (page == null) {
+            throw new IllegalArgumentException("page is null");
+        } else {
+            PageData<T> pageData = new PageData<>();
+            pageData.setCurrentPage(page.getCurrent());
+            pageData.setPageSize(page.getSize());
+            pageData.setTotal(page.getTotal());
+            pageData.setRecords(page.getRecords());
+            return pageData;
+        }
+    }
+}
